bf60ed4eab516a932bc548ee1793947528e085ff
reebxu459 <rebbi.xu123@gmail.com>
2023-01-09 19:13:22 -0500

    done contact jsx component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
-import {RiMessengerLine} from 'react-icons/ri'
+import {AiFillMessage} from 'react-icons/ai'
 
 const Contact = () => {
   return (
@@ -18,12 +18,17 @@ const Contact = () => {
             <a href="mailto:r25xu@uwaterloo.ca">Send a message</a>
           </article>
           <article className="contact__option">
-            <RiMessengerLine/>
-            <h4>Email</h4>
-            <h5>r25xu@uwaterloo.ca</h5>
-            <a href="mailto:r25xu@uwaterloo.ca">Send a message</a>
+            <AiFillMessage/>
+            <h4>Message</h4>
+            <h5>(+1)647-303-3508</h5>
           </article>
         </div>
+        <form action="">
+          <input type = "text" name='name' placeholder= 'Your Full Name' required />
+          <input type = "email" name='email' placeholder= 'Your Email' required />
+          <textarea name="message" rows="7" placeholder= 'Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form>
       </div>
     </section>
   )
