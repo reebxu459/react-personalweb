f10e975ea0d99f188ce53002928afa0b489e774f
reebxu459 <rebbi.xu123@gmail.com>
2023-01-11 14:25:34 -0500

    tweaks

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,8 +5,9 @@ import {BsPatchCheckFill} from 'react-icons/bs'
 const Experience = () => {
   return (
     <section id='experience'>
-      <h5>Skills</h5>
-      <h2>My Experience</h2>
+
+        <h5>Skills</h5>
+        <h2>My Experience</h2>
 
       <div className="container experience__container">
         <div className="experience__frontend">
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,7 +12,7 @@ const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello, I'm</h5>
+        <h4>Hello, I'm</h4>
         <h1>Rebecca.</h1>
         <div className="typewriter">
           <TypeWriterEffect
